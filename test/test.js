6e672dc73c520b2e390a7e99df5da9120c850e99
Xun <xunchen369@gmail.com>
2016-07-13 12:15:11 -0700

    developing unspread

--- test/test.js
+++ test/test.js
@@ -16,80 +16,156 @@ var complexObjOutput = require('./output/complexObj');
 var complexArrOutput = require('./output/complexArr');
 var reallifeOutput = require('./output/reallife');
 
-var jsonSpread = require('../index.js');
+var jsonSpread = require('../src/index.js');
 
 var chai = require('chai');
 var assert = chai.assert;
 var expect = chai.expect;
 
-describe('Input', function(){
-  it('should throw Error when input is not Object or Array' ,function () {
-    expect(function(){jsonSpread("string")}).to.throw(TypeError);
-    expect(function(){jsonSpread(1)}).to.throw(TypeError);
+describe('Spread', function(){
+  describe('Input', function(){
+    it('should throw Error when input is not Object or Array' ,function () {
+      expect(function(){jsonSpread("string")}).to.throw(TypeError);
+      expect(function(){jsonSpread(1)}).to.throw(TypeError);
+    });
+    it('should not throw Error when input is Object or Array' ,function () {
+      expect(function(){jsonSpread({})}).to.not.throw(TypeError);
+      expect(function(){jsonSpread([])}).to.not.throw(TypeError);
+    });
   });
-  it('should not throw Error when input is Object or Array' ,function () {
-    expect(function(){jsonSpread({})}).to.not.throw(TypeError);
-    expect(function(){jsonSpread([])}).to.not.throw(TypeError);
-  });
-});
 
-describe('Output', function(){
-  describe("Multiple Array", function(){
-    it('input should be equal to output json' , function(){ 
-      assert.strictEqual(JSON.stringify(jsonSpread(multipleArrInput)),JSON.stringify(multipleArrOutput));
+  describe('Output', function(){
+    describe("Multiple Array", function(){
+      it('input should be equal to output json' , function(){ 
+        assert.strictEqual(JSON.stringify(jsonSpread(multipleArrInput)),JSON.stringify(multipleArrOutput));
+      })
+    })
+    describe("Nested Array", function(){
+      it('input should be equal to output json' , function(){ 
+        assert.strictEqual(JSON.stringify(jsonSpread(nestedArrInput)),JSON.stringify(nestedArrOutput));
+      })
+    })
+    describe("Nested Object", function(){
+      it('input should be equal to output json' , function(){ 
+        assert.strictEqual(JSON.stringify(jsonSpread(nestedObjInput)),JSON.stringify(nestedObjOutput));
+      })
+    })
+    describe("Nested Array Objects", function(){
+      it('input should be equal to output json' , function(){ 
+        assert.strictEqual(JSON.stringify(jsonSpread(nestedArrObjInput)),JSON.stringify(nestedArrObjOutput));
+      })
+    })
+    describe("Nested Object Arrays", function(){
+      it('input should be equal to output json' , function(){ 
+        assert.strictEqual(JSON.stringify(jsonSpread(nestedObjArrInput)),JSON.stringify(nestedObjArrOutput));
+      })
+    })
+    describe("Complex Object", function(){
+      it('input should be equal to output json' , function(){
+        assert.strictEqual(JSON.stringify(jsonSpread(complexObjInput)),JSON.stringify(complexObjOutput));
+      })
+    })
+    describe("Complex Array", function(){
+      it('input should be equal to output json' , function(){
+        assert.strictEqual(JSON.stringify(jsonSpread(complexArrInput)),JSON.stringify(complexArrOutput));
+      })
+    })
+    describe("Real Life Example", function(){
+      it('input should be equal to output json' , function(){
+        assert.strictEqual(JSON.stringify(jsonSpread(reallifeInput)),JSON.stringify(reallifeOutput));
+      })
     })
   })
-  describe("Nested Array", function(){
-    it('input should be equal to output json' , function(){ 
-      assert.strictEqual(JSON.stringify(jsonSpread(nestedArrInput)),JSON.stringify(nestedArrOutput));
+
+  describe('Options', function(){
+    describe("Remove Empty Arrays" , function(){
+      it("should remove empty array fields", function(){
+        assert.strictEqual(JSON.stringify(jsonSpread({a: 1, b:[]},{removeEmptyArray: true})),JSON.stringify([{a: 1}]));
+      })
     })
-  })
-  describe("Nested Object", function(){
-    it('input should be equal to output json' , function(){ 
-      assert.strictEqual(JSON.stringify(jsonSpread(nestedObjInput)),JSON.stringify(nestedObjOutput));
+    describe("Empty Arrays Value" , function(){
+      it("should replace empty array fields with 'EMPTY'", function(){
+        assert.strictEqual(JSON.stringify(jsonSpread({a: 1, b:[]},{emptyValue: "EMPTY"})),JSON.stringify([{a: 1, b: "EMPTY"}]));
+      })
     })
-  })
-  describe("Nested Array Objects", function(){
-    it('input should be equal to output json' , function(){ 
-      assert.strictEqual(JSON.stringify(jsonSpread(nestedArrObjInput)),JSON.stringify(nestedArrObjOutput));
-    })
-  })
-  describe("Nested Object Arrays", function(){
-    it('input should be equal to output json' , function(){ 
-      assert.strictEqual(JSON.stringify(jsonSpread(nestedObjArrInput)),JSON.stringify(nestedObjArrOutput));
-    })
-  })
-  describe("Complex Object", function(){
-    it('input should be equal to output json' , function(){
-      assert.strictEqual(JSON.stringify(jsonSpread(complexObjInput)),JSON.stringify(complexObjOutput));
-    })
-  })
-  describe("Complex Array", function(){
-    it('input should be equal to output json' , function(){
-      assert.strictEqual(JSON.stringify(jsonSpread(complexArrInput)),JSON.stringify(complexArrOutput));
-    })
-  })
-  describe("Real Life Example", function(){
-    it('input should be equal to output json' , function(){
-      assert.strictEqual(JSON.stringify(jsonSpread(reallifeInput)),JSON.stringify(reallifeOutput));
+    describe("Delimiter" , function(){
+      it("should use '*' as delimiters", function(){
+        assert.strictEqual(JSON.stringify(jsonSpread({a: { b: 1 }},{delimiter: "*"})),JSON.stringify([{"a*b": 1}]));
+      })
     })
   })
 })
 
-describe('Options', function(){
-  describe("Remove Empty Arrays" , function(){
-    it("should remove empty array fields", function(){
-      assert.strictEqual(JSON.stringify(jsonSpread({a: 1, b:[]},{removeEmptyArray: true})),JSON.stringify([{a: 1}]));
+describe('Unspread', function(){
+  describe('Input', function(){
+    it('should throw Error when input is not Object or Array' ,function () {
+      expect(function(){jsonSpread.unspread("string")}).to.throw(TypeError);
+      expect(function(){jsonSpread.unspread(1)}).to.throw(TypeError);
+    });
+    it('should not throw Error when input is Object or Array' ,function () {
+      expect(function(){jsonSpread.unspread({})}).to.not.throw(TypeError);
+      expect(function(){jsonSpread.unspread([])}).to.not.throw(TypeError);
+    });
+  });
+
+  describe('Output', function(){
+    describe("Multiple Array", function(){
+      it('input should be equal to output json' , function(){ 
+        assert.strictEqual(JSON.stringify(jsonSpread.unspread(multipleArrOutput)),JSON.stringify([ multipleArrInput ]));
+      })
+    })
+    describe("Nested Array", function(){
+      it('input should be equal to output json' , function(){ 
+        assert.strictEqual(JSON.stringify(jsonSpread.unspread(nestedArrOutput)),JSON.stringify([ nestedArrInput ]));
+      })
+    })
+    describe("Nested Object", function(){
+      it('input should be equal to output json' , function(){ 
+        assert.strictEqual(JSON.stringify(jsonSpread.unspread(nestedObjOutput)),JSON.stringify([ nestedObjInput ]));
+      })
+    })
+    describe("Nested Array Objects", function(){
+      it('input should be equal to output json' , function(){ 
+        assert.strictEqual(JSON.stringify(jsonSpread.unspread(nestedArrObjOutput),"a.index"),JSON.stringify([ nestedArrObjInput ]));
+      })
+    })
+    describe("Nested Object Arrays", function(){
+      it('input should be equal to output json' , function(){ 
+        assert.strictEqual(JSON.stringify(jsonSpread.unspread(nestedObjArrOutput)),JSON.stringify([ nestedObjArrInput ]));
+      })
+    })
+    describe("Complex Object", function(){
+      it('input should be equal to output json' , function(){
+        assert.strictEqual(JSON.stringify(jsonSpread.unspread(complexObjOutput)),JSON.stringify([ complexObjInput ]));
+      })
+    })
+    describe("Complex Array", function(){
+      it('input should be equal to output json' , function(){
+        assert.strictEqual(JSON.stringify(jsonSpread.unspread(complexArrOutput)),JSON.stringify(complexArrInput));
+      })
+    })
+    describe("Real Life Example", function(){
+      it('input should be equal to output json' , function(){
+        assert.strictEqual(JSON.stringify(jsonSpread.unspread(reallifeOutput)),JSON.stringify(reallifeInput));
+      })
     })
   })
-  describe("Empty Arrays Value" , function(){
-    it("should replace empty array fields with 'EMPTY'", function(){
-      assert.strictEqual(JSON.stringify(jsonSpread({a: 1, b:[]},{emptyValue: "EMPTY"})),JSON.stringify([{a: 1, b: "EMPTY"}]));
+
+  describe('Options', function(){
+    describe("Remove Empty Arrays" , function(){
+      it("should remove empty array fields", function(){
+        assert.strictEqual(JSON.stringify(jsonSpread.unspread({a: 1, b:[]},{removeEmptyArray: true})),JSON.stringify([{a: 1}]));
+      })
     })
-  })
-  describe("Delimiter" , function(){
-    it("should use '*' as delimiters", function(){
-      assert.strictEqual(JSON.stringify(jsonSpread({a: { b: 1 }},{delimiter: "*"})),JSON.stringify([{"a*b": 1}]));
+    describe("Empty Arrays Value" , function(){
+      it("should replace empty array fields with 'EMPTY'", function(){
+        assert.strictEqual(JSON.stringify(jsonSpread.unspread({a: 1, b:[]},{emptyValue: "EMPTY"})),JSON.stringify([{a: 1, b: "EMPTY"}]));
+      })
+    })
+    describe("Delimiter" , function(){
+      it("should use '*' as delimiters", function(){
+        assert.strictEqual(JSON.stringify(jsonSpread.unspread({"a*b": 1},{delimiter: "*"})),JSON.stringify([{a: { b: 1 }}]));
+      })
     })
   })
 })
