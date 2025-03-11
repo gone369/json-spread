4e3fe3f483ae8320c7120de849cb2be32a5304da
Nick.Zhou <zhoujie@555class.com>
2025-03-11 14:00:54 +0800

    feat: Add TypeScript type definition support

---
+++ index.d.ts
@@ -0,0 +1,55 @@
+declare namespace JsonSpread {
+  interface Options {
+    /**
+     * Delimiter used for flattening nested object keys
+     * @default "."
+     */
+    delimiter?: string;
+    
+    /**
+     * Whether to remove empty arrays
+     * @default false
+     */
+    removeEmptyArray?: boolean;
+    
+    /**
+     * Replacement value for empty arrays
+     * @default null
+     */
+    emptyValue?: any;
+    
+    /**
+     * Whether to use safe mode
+     * @default true
+     */
+    safe?: boolean;
+    
+    /**
+     * Whether to enable debug mode
+     * @default false
+     */
+    debug?: boolean;
+  }
+
+  /**
+   * Spreads nested JSON or JavaScript objects into a flat structure
+   * @param input Input object or array of objects
+   * @param options Configuration options
+   * @returns Array of flattened objects
+   */
+  function spread<T = any>(input: object | object[], options?: Options): T[];
+}
+
+/**
+ * Spreads nested JSON or JavaScript objects into a flat structure
+ * @param input Input object or array of objects
+ * @param options Configuration options
+ * @returns Array of flattened objects
+ */
+declare function jsonSpread<T = any>(input: object | object[], options?: JsonSpread.Options): T[];
+
+declare namespace jsonSpread {
+  export const spread: typeof JsonSpread.spread;
+}
+
+export = jsonSpread; 
